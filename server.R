0b9a6fbd20efd17fa1fa93f4e84cf793921b0af1
mclapham <mclapham@ucsc.edu>
2014-09-12 15:46:31 -0700

    Update server.R

--- server.R
+++ server.R
@@ -1,31 +1,83 @@
 library(shiny)
 
-# Define UI for dataset viewer application
-shinyUI(fluidPage(
-  
-  # Application title.
-  titlePanel("First Appearances Calculator"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      h3("Find the oldest representative of a group, based on data from the Paleobiology Database"),
-      textInput("taxon", "Enter a taxon name:","Omaliinae"),
-      
-      helpText("Warning: large taxonomic groups may require
-               a few seconds (or minutes) to load."),
-      
-      submitButton("Submit")
-                  
-    ),
+#reads time interval names, ages, colors
+time_int<-read.csv("http://paleobiodb.org/data1.1/intervals/list.txt?scale=1&limit=all")
+periods<-subset(time_int,time_int$level==3)
 
-    mainPanel(
-      tabsetPanel(type = "tabs", 
-                  tabPanel("Plot", plotOutput("plot")), 
-                  tabPanel("Table", h3("Oldest occurrences"),
-                           tableOutput("table1"),
-                           h3("Oldest occurrences of classified species"),
-                           tableOutput("table2"))
-        )
-    )
+# Define server logic for random distribution application
+shinyServer(function(input, output) {
+      
+  dataInput<-reactive({
+    occurrences<-read.csv(paste("http://paleobiodb.org/data1.1/occs/list.txt?base_name=",input$taxon,"&show=ident&limit=99999",sep=""))
+    
+    occurrences$mean_age<-rowMeans(cbind(occurrences$early_age,occurrences$late_age))
+    
+    return(occurrences)
+
+  })
+  
+  
+  output$plot<-renderPlot({
+    dataset<-dataInput()
+    hist_output<-hist(dataset$mean_age,breaks=seq(0,545,by=1),plot=F)
+    hist_data<-data.frame(counts=hist_output$counts,mids=hist_output$mids)
+    hist_data<-subset(hist_data,hist_data$counts>0)
+    layout(matrix(c(1,2),ncol=2),widths=c(1,5))
+    par(mar=c(5,4,1.25,0))
+    plot(0,0,type="n",xlim=c(0,5),xaxt="n",xlab="",ylab="Age (Ma)",ylim=c(max(hist_data$mids),min(hist_data$mids)),bty="n")
+    rect(0,periods$early_age,5,periods$late_age,col=paste(periods$color))
+    text(rep(2.5,nrow(periods)),rowMeans(cbind(periods$early_age,periods$late_age)),periods$abbrev)
+    mtext(input$taxon,side=3,adj=0,cex=1.5)
+    par(mar=c(5,0,1.25,1))
+    plot(hist_data$counts,hist_data$mids,type="n",xlab="Number of occurrences",xlim=c(0,max(hist_data$counts)),ylim=c(max(hist_data$mids),min(hist_data$mids)),yaxt="n",ylab="",bty="n")
+    segments(rep(0,length(hist_data$mids)),hist_data$mids,hist_data$counts,hist_data$mids,lwd=3,col="steelblue3")
+  })
+  
+  output$downloadData <- downloadHandler(
+    
+    filename = function() {
+      paste(input$taxon, ".csv", sep = "")
+    },
+    
+    content = function(file) {
+      occurrences<-dataInput()
+      write.csv(occurrences,file,row.names = FALSE)
+      }
   )
-))
+  
+  
+  maxOcc<-reactive({
+    dataset<-dataInput()
+    subset(dataset,dataset$mean_age==max(dataset$mean_age))
+  })
+  
+  maxOccSp<-reactive({
+    dataset<-dataInput()
+    species<-subset(dataset,dataset$matched_rank==3)
+    species<-subset(species,species$genus_reso=="" | species$genus_reso=="n. gen.")
+    subset(species,species$mean_age==max(species$mean_age))
+  })
+  
+  output$table1<-renderTable({
+    max_occ<-maxOcc()
+    data.frame(taxon=max_occ$matched_name,
+               collection=max_occ$collection_no,
+               early_interval=max_occ$early_interval,
+               max_age=max_occ$early_age,
+               late_interval=max_occ$late_interval,
+               min_age=max_occ$late_age
+               )
+    })
+    
+    
+   output$table2<-renderTable({
+      max_occ<-maxOccSp()
+      data.frame(taxon=max_occ$matched_name,
+                 collection=max_occ$collection_no,
+                early_interval=max_occ$early_interval,
+                 max_age=max_occ$early_age,
+                 late_interval=max_occ$late_interval,
+                 min_age=max_occ$late_age
+                  )
+    })  
+})
