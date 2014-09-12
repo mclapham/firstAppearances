6f089bb4e39af7b5a337ddcad1d12a86d09dd42f
mclapham <mclapham@ucsc.edu>
2014-09-12 15:46:13 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -8,18 +8,26 @@ shinyUI(fluidPage(
   
   sidebarLayout(
     sidebarPanel(
+      h4("Find the oldest representative of a group, based on data from the Paleobiology Database"),
+      
       textInput("taxon", "Enter a taxon name:","Omaliinae"),
       
       helpText("Warning: large taxonomic groups may require
-               a few seconds (or minutes) to load."),
+               a few seconds (or more) to load."),
       
-      submitButton("Submit")
-              
+      submitButton("Submit"),
+      
+      h5("If you use this information in a publication, please acknowledge the Paleobiology Database"),
+      
+      h6("Comments or suggestions? Email mclapham@ucsc.edu")
+                  
     ),
 
     mainPanel(
       tabsetPanel(type = "tabs", 
-                  tabPanel("Plot", plotOutput("plot")), 
+                  tabPanel("Plot", plotOutput("plot"),
+                           downloadButton("downloadData", "Download occurrences")
+                           ), 
                   tabPanel("Table", h3("Oldest occurrences"),
                            tableOutput("table1"),
                            h3("Oldest occurrences of classified species"),
